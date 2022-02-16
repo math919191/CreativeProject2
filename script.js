17b9a0da0213fb256ee7c840ef32963d1a7115df
math919191 <math919191@gmail.com>
2022-02-15 17:48:27 -0700

    started...

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("test").innerText = "hello work";
+            
+
+//function useful(){
+    // // const myurl = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+    // // console.log(myurl);
+    // fetch(myurl)
+    //     .then(function(response) {
+    //         return response.json();
+    //     }).then(function(json){
+    //         //console.log(json);
+    //         let myJoke = json.joke; 
+            
+  //          document.getElementById("test").innerText = "hello work";
+            
+        
+            //console.log(myJoke);
+            // let text = document.createTextNode(json.joke);
+            // const element = document.getElementById("joke");
+            // element.appendChild(text);
+
+        //})
+
+//}
