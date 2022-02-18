f7466231993c3a70dfc8d01aac97013c4c646521
math919191 <math919191@gmail.com>
2022-02-17 17:23:38 -0700

    google API starting to work

--- googlebooks.js
+++ googlebooks.js
@@ -1,31 +1,33 @@
 //https://developers.google.com/books/docs/v1/using#APIKey
-//
+//https://developers.google.com/books/docs/v1/libraries
 let KEY= "AIzaSyC3b16m7c_Z258vd4Q-KlwVcoH__WIJa44";
 
 
 
-function makeCard(isbn, title, description){
+function makeCardGoogle(title, description){
 
     let card = "";
     card += '<div class="book">';
     card += '<div class="rec-book">';
-    card += "<img src='https://covers.openlibrary.org/b/isbn/";
-    card += isbn;
-    card += "-M.jpg' />";
+    
+    //card += "<img src='https://covers.openlibrary.org/b/isbn/";
+    //card += isbn;
+    //card += "-M.jpg' />";
 
     card += '</div><div class="description">';
     card += '<h6>' + title + '</h6>';
     card +=  '<p>' + description + '</p>';
     card += '</div></div>';
-
     return card;
-
 
 }
 
-function useful(isbn){
-
-    const myurl = "https://openlibrary.org/isbn/" + isbn + ".json";
+function usefulGoogle(){
+    console.log("here");
+    let descript = "huck";
+    let author = "twain";
+    //const myurl = "https://openlibrary.org/isbn/" + isbn + ".json";
+    let myurl = "https://www.googleapis.com/books/v1/volumes?q=" + descript + "+inauthor:" + author + "&key=" + KEY;
 
     console.log(myurl);
     fetch(myurl)
@@ -34,31 +36,31 @@ function useful(isbn){
         }).then(function(json){
             console.log(json);
 
-
+            //  let description = json.description; 
+            //  console.log(description);
+            //  let results = "";
+            //  results += '<h2>Weather in ' + json.description + "</h2>";
         
+            //  results += "<p>";
+            //  results += description;
+            //  results += "</p>";
+            //  results += "<img src='https://covers.openlibrary.org/b/isbn/";
+            //  results += isbn;
 
-
-             let description = json.description; 
-             console.log(description);
-             let results = "";
-             results += '<h2>Weather in ' + json.description + "</h2>";
-        
-             results += "<p>";
-             results += description;
-             results += "</p>";
-             results += "<img src='https://covers.openlibrary.org/b/isbn/";
-             results += isbn;
-
-             results += "-M.jpg' />";
+            //  results += "-M.jpg' />";
 
              //document.getElementById("test").innerHTML = results;
 
             
-             let card = makeCard(isbn, json.title, description);
-             document.getElementById("test").innerHTML = card;
+             //let card = makeCard(isbn, json.title, description);
+             console.log(json.items[0].volumeInfo.title);
+             console.log(json.items[0].volumeInfo.authors);
+             
+             let card = makeCardGoogle(json.items[0].volumeInfo.title, json.items[0].volumeInfo.authors);
+             
+             document.getElementById("test2").innerHTML = card;
         })
 
 }
 
-//useful(9780812994520);
-useful(9780747532743);
+usefulGoogle();
