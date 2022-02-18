d59382847f957aba5af9546291d912663d59317a
math919191 <math919191@gmail.com>
2022-02-17 17:09:45 -0700

    started google stuff

---
+++ googlebooks.js
@@ -0,0 +1,64 @@
+//https://developers.google.com/books/docs/v1/using#APIKey
+//
+let KEY= "AIzaSyC3b16m7c_Z258vd4Q-KlwVcoH__WIJa44";
+
+
+
+function makeCard(isbn, title, description){
+
+    let card = "";
+    card += '<div class="book">';
+    card += '<div class="rec-book">';
+    card += "<img src='https://covers.openlibrary.org/b/isbn/";
+    card += isbn;
+    card += "-M.jpg' />";
+
+    card += '</div><div class="description">';
+    card += '<h6>' + title + '</h6>';
+    card +=  '<p>' + description + '</p>';
+    card += '</div></div>';
+
+    return card;
+
+
+}
+
+function useful(isbn){
+
+    const myurl = "https://openlibrary.org/isbn/" + isbn + ".json";
+
+    console.log(myurl);
+    fetch(myurl)
+        .then(function(response) {
+            return response.json();
+        }).then(function(json){
+            console.log(json);
+
+
+        
+
+
+             let description = json.description; 
+             console.log(description);
+             let results = "";
+             results += '<h2>Weather in ' + json.description + "</h2>";
+        
+             results += "<p>";
+             results += description;
+             results += "</p>";
+             results += "<img src='https://covers.openlibrary.org/b/isbn/";
+             results += isbn;
+
+             results += "-M.jpg' />";
+
+             //document.getElementById("test").innerHTML = results;
+
+            
+             let card = makeCard(isbn, json.title, description);
+             document.getElementById("test").innerHTML = card;
+        })
+
+}
+
+//useful(9780812994520);
+useful(9780747532743);
